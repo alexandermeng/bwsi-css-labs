a35c1f93ae9ddd849074b90ec01ee532ed993c3b
alexandermeng <ameng27@nobles.edu>
2026-02-20 19:54:23 -0500

    Added original course introduction to lab_1a

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "" # TODO: Insert your name between the double quotes
 
     print(f"Alexander Meng, Welcome to the CSS course!")
+    print(f"Hi, my name is Alexander, and I'm a junior from Massachusetts. Right now, I play more Clash Royale than chess, but over the summer, I reached 2680 bullet on chess.com and used to be 2nd on the global leaderboard for a variant called crazyhouse.")
 
 if __name__ == "__main__":
     main()
